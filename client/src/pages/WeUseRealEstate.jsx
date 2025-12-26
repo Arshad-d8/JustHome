0b0c97326911dfaec8d76a2eb4231e93a6e840d2
Arshad-d8 <71703633+Arshad-d8@users.noreply.github.com>
2025-12-26 16:40:18 +0530

    updated

--- client/src/pages/WeUseRealEstate.jsx
+++ client/src/pages/WeUseRealEstate.jsx
@@ -6,7 +6,7 @@ export default function WeUseRealEstate() {
     return (
         <div className='flex flex-col md:flex-row justify-between md:h-screen px-10 md:px-35 my-20 gap-20'>
             <div className='flex flex-col justify-center text-center md:text-start items-center md:items-start gap-10 md:w-1/2'>
-                <p className='text-4xl font-semibold'>We Use Real Estate to Show <br />Our Appreciation of The <br />World</p>
+                <p className='text-2xl sm:text-4xl md:text-4xl font-semibold'>We Use Real Estate to Show <br />Our Appreciation of The <br />World</p>
                 <p className='text-sm'>Pellentesque egestas elementum egestas faucibus sem. Velit nunc egestas ut morbi. Leo diam diam nibh eget fermentum massa pretium. Mi mauris nulla ac dictum ut mauris non.</p>
                 <div className='grid grid-cols-2 gap-10 md:gap-0 md:gap-y-5 '>
                     <div className='flex flex-col gap-3'>
